48c08dc0fecf27fe1660e2a1320c7e58e108b548
adarshRamphale <adarsh.ramphale@humancloud.co.in>
2025-01-17 18:00:41 +0530

    Write just enough code to make the empty string test pass

--- src/main/java/com/stringCalculatorAssesment/StringCalculatorAssesment.java
+++ src/main/java/com/stringCalculatorAssesment/StringCalculatorAssesment.java
@@ -3,6 +3,9 @@ package com.stringCalculatorAssesment;
 public class StringCalculatorAssesment {
 
     public int add(String numbers) {
+        if(numbers.isEmpty()){
+            return 0;
+        }
         return -1;
     }
 }
